5839e03ddca15214a37a9a2f8db76a3b31f4a86f
skyluke01 <72421889+skyluke01@users.noreply.github.com>
2020-11-10 19:18:26 -0300

    Update BandDetailsPage.js

--- src/pages/BandDetailsPage.js
+++ src/pages/BandDetailsPage.js
@@ -10,21 +10,21 @@ export default class BandDetailsPage extends React.Component{
     }
     render(){
         return(
-            <ScrollView style = {style.container}>
+            <ScrollView style = {style.scroll}>
             <SafeAreaView>
             <Image style = {style.avatar} source = {{uri: this.state.band.imagem}} />
-            <Text style = {style.baseText}>Nome:</Text>
-            <Text style = {style.baseText}>{this.state.band.nome.banda}</Text>
-            <Text style = {style.baseText}>Sigla: </Text>
-            <Text style = {style.baseText}> {this.state.band.nome.sigla}</Text>
-            <Text style = {style.baseText}>Gravadora:</Text>
-            <Text style = {style.baseText}>{this.state.band.gravadora}</Text>
-            <Text style = {style.baseText}>Gênero:</Text>
-            <Text style = {style.baseText}> {this.state.band.genero}</Text>
-            <Text style = {style.baseText}>País:</Text>
-            <Text style = {style.baseText}>{this.state.band.país}</Text>
-            <Text style = {style.baseText}>Lider:</Text>
-            <Text style = {style.baseText}>{this.state.band.lider}</Text>
+            <Text style = {style.texto}>Nome:</Text>
+            <Text style = {style.texto}>{this.state.band.nome.banda}</Text>
+            <Text style = {style.texto}>Sigla: </Text>
+            <Text style = {style.texto}> {this.state.band.nome.sigla}</Text>
+            <Text style = {style.texto}>Gravadora:</Text>
+            <Text style = {style.texto}>{this.state.band.gravadora}</Text>
+            <Text style = {style.texto}>Gênero:</Text>
+            <Text style = {style.texto}> {this.state.band.genero}</Text>
+            <Text style = {style.texto}>País:</Text>
+            <Text style = {style.texto}>{this.state.band.país}</Text>
+            <Text style = {style.texto}>Lider:</Text>
+            <Text style = {style.texto}>{this.state.band.lider}</Text>
             </SafeAreaView>
             </ScrollView>
         )
@@ -32,20 +32,16 @@ export default class BandDetailsPage extends React.Component{
 }
 const style = StyleSheet.create(
     {
-      container: {
+      scroll: {
         flex: 1,
         backgroundColor: '#f5f5dc',  
       },
       line: {
         height: 60, borderBottomWidth: 1, borderBottomColor: '#37276B', alignItems: 'center', flexDirection: 'row'
     },
-    baseText: {
+    texto: {
         fontFamily: "Cochin", fontSize: 30, textAlign: "center"
         
-      },
-      titleText: {
-        fontSize: 60, 
-        fontWeight: "bold"
       },
       avatar: {
         aspectRatio: 1, margin: 70, height: 250,
